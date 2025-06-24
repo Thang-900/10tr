6c8a95883278df083fbaadba3a8bd2e0e696487e
Thang-900 <hmanhthang41@gmail.com>
2025-06-24 14:36:16 +0700

    thiếu

--- game10tr/Assets/_Script/2.animations/AnimationTransitions.cs
+++ game10tr/Assets/_Script/2.animations/AnimationTransitions.cs
@@ -7,33 +7,7 @@ public class transitions : MonoBehaviour
 {
     // phải lấy input vertical và input horizontal trong hàm tổng còn x,y ở trong này là hướng.
     // phải thiết lập animator cho từng vũ khí khi đứng yên, di chuyển, không nên dùng chung
-    public void Transition(GameObject up,GameObject down,GameObject side, float x, float y)
-    {
-        
-        
-        if ((x == 1 || x == -1) && y == 0)
-        {
-            side.SetActive(true);
-            up.SetActive(false);
-            down.SetActive(false);
-        }
-        //directionX == 0 ||directionX==1||directionX==-1 && 
-        //hien bom dang di len
-        else if (y == 1)
-        {
-            side.SetActive(false);
-            up.SetActive(true);
-            down.SetActive(false);
-        }
-        //directionX == 0 && 
-        //hien bom dang di xuong
-        else if (y == -1)
-        {
-            up.SetActive(false);
-            side.SetActive(false);
-            down.SetActive(true);
-        }
-        
-    }
+    
+    
 }
 
